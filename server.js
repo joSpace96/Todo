3d1dbe41a749181c63ec3f94cae1c2692207b3ff
spaceJo96 <joalstn11@naver.com>
2023-04-21 23:36:30 +0900

    폼에 입력한 정보 서버에 POST로 전송

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-  console.log("Server listening on");
+app.listen(3000, function () {
+  console.log("listening on 3000");
 });
 
-app.get("/pet", (req, res) => {
-  res.send("<h1>댕댕이와 냐옹이</h1>");
+app.get("/", function (res, req) {
+  req.sendFile(__dirname + "/index.html");
 });
 
-app.get("/beauty", (req, res) => {
-  res.send("<h1>뷰티용품을 쇼핑할수 있는 페이지 입니다용</h1>");
+app.get("/write", function (res, req) {
+  req.sendFile(__dirname + "/write.html");
 });
 
-app.get("/beauty", (req, res) => {
-  res.send("<h1>뷰티용품을 쇼핑할수 있는 페이지 입니다용</h1>");
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.get("/write", (req, res) => {
-  res.sendFile(__dirname + "/write.html");
+app.post("/add", function (res, req) {
+  console.log(res.body);
+  req.send("전송완료");
 });
