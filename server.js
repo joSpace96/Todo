592d32d9c88b0bff68f33c0147bd4ddbfb8a4958
spaceJo96 <joalstn11@naver.com>
2023-04-21 21:44:47 +0900

    Express설치 후 서버가동

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server listening on");
+});
+
+app.get("/pet", (req, res) => {
+  res.send("<h1>댕댕이와 냐옹이</h1>");
+});
+
+app.get("/beauty", (req, res) => {
+  res.send("<h1>뷰티용품을 쇼핑할수 있는 페이지 입니다용</h1>");
+});
