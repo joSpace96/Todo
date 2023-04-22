d0a85638f83a88198421b27b4ace8c82c49e0ef0
spaceJo96 <joalstn11@naver.com>
2023-04-22 16:29:56 +0900

    mongodb 연결

--- server.js
+++ server.js
@@ -3,19 +3,27 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function () {
-  console.log("listening on 3000");
-});
+const MongoClient = require("mongodb").MongoClient;
+MongoClient.connect(
+  "mongodb+srv://admin:<1q2w3e4r>@cluster0.yvz01u3.mongodb.net/?retryWrites=true&w=majority",
+  (err, client) => {
+    app.listen(3000, () => {
+      console.log("listening on 3000");
+    });
+  }
+);
 
-app.get("/", function (res, req) {
+app.get("/", (res, req) => {
   req.sendFile(__dirname + "/index.html");
 });
 
-app.get("/write", function (res, req) {
+app.get("/write", (res, req) => {
   req.sendFile(__dirname + "/write.html");
 });
 
-app.post("/add", function (res, req) {
-  console.log(res.body);
+app.post("/add", (res, req) => {
+  console.log(res.body.date);
+  console.log(res.body.title);
+
   req.send("전송완료");
 });
