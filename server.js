81d9e78a0d6c4a00180ef11554117a10f6a38d55
spaceJo96 <joalstn11@naver.com>
2023-04-23 15:51:23 +0900

    DB에 데이터저장

--- server.js
+++ server.js
@@ -3,10 +3,23 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// mongodb 연결
+var db;
 const MongoClient = require("mongodb").MongoClient;
 MongoClient.connect(
-  "mongodb+srv://admin:<1q2w3e4r>@cluster0.yvz01u3.mongodb.net/?retryWrites=true&w=majority",
+  "mongodb+srv://admin:1q2w3e4r@cluster0.yvz01u3.mongodb.net/?retryWrites=true&w=majority",
+  { useUnifiedTopology: true },
   (err, client) => {
+    if (err) return console.log(err);
+    db = client.db("todo");
+
+    db.collection("post").insertOne(
+      { name: "Jo", age: 28, _id: 99 },
+      (err, data) => {
+        console.log("저장완료");
+      }
+    );
+
     app.listen(3000, () => {
       console.log("listening on 3000");
     });
@@ -24,6 +37,14 @@ app.get("/write", (res, req) => {
 app.post("/add", (res, req) => {
   console.log(res.body.date);
   console.log(res.body.title);
-
   req.send("전송완료");
+  db.collection("post").insertOne(
+    {
+      title: res.body.title,
+      date: res.body.date,
+    },
+    (err, data) => {
+      console.log("저장완료");
+    }
+  );
 });
