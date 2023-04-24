482e8051c6139963a611b64306527bb5ef3dd152
spaceJo96 <joalstn11@naver.com>
2023-04-24 17:11:40 +0900

    html파일들 ejs로 변경 nav 파일 따로만들어서 적용

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+app.use("/public", express.static("public"));
+
 // mongodb 연결
 var db;
 const MongoClient = require("mongodb").MongoClient;
@@ -19,12 +21,12 @@ MongoClient.connect(
   }
 );
 
-app.get("/", (res, req) => {
-  req.sendFile(__dirname + "/index.html");
+app.get("/", (req, res) => {
+  res.render("index.ejs", {});
 });
 
-app.get("/write", (res, req) => {
-  req.sendFile(__dirname + "/write.html");
+app.get("/write", (req, res) => {
+  res.render("write.ejs", {});
 });
 
 app.post("/add", (res, req) => {
